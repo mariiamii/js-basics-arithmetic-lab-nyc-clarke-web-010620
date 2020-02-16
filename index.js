096db48201ad1cf65e19122ead86dff297c93b9e
Mariam Anuashvili <m.anuashvili@gmail.com>
2020-02-16 19:45:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,12 +7,6 @@ const random = Math.random();
 
 const mod = 14 % 5; 
 
-// describe('mod', function() {
-//     it("is generates a remainder that is equal to 4", function() {
-//       expect(mod).to.eq(4)
-//     });
-//   });
 
 
-// Create a variable called mod set to an equation whose remainder is equal to "4".
 // Create a variable called max that will return "20" as the highest number in the set.
