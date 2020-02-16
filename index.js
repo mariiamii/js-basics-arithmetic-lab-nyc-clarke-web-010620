c98296062f56eae548233de6997f61faa8dcbbf0
Mariam Anuashvili <m.anuashvili@gmail.com>
2020-02-16 19:43:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ let var2 = 31;
 const multiply = var1 * var2;
 
 const random = Math.random();
-// describe('random', function() {
-//     it("is generates a random integer greater than 0", function() {
-//       expect(random).to.be.a('number')
-//       expect(random).to.not.eq(0)
+
+const mod = 25 % 3; 
+
+// describe('mod', function() {
+//     it("is generates a remainder that is equal to 4", function() {
+//       expect(mod).to.eq(4)
 //     });
 //   });
 
-// Create a variable called multiply that will multiply var1 and var2 whose results will equal "62".
-// Create a variable called random that will generate a random integer greater than 0.
+
 // Create a variable called mod set to an equation whose remainder is equal to "4".
 // Create a variable called max that will return "20" as the highest number in the set.
