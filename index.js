645a7a1ab67974e8dcca986964543da8fcbe579b
Mariam Anuashvili <m.anuashvili@gmail.com>
2020-02-16 19:39:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ let var1 = 2
 let var2 = 31
 const multiply = var1 * var2
 
+// Create a variable called multiply that will multiply var1 and var2 whose results will equal "62".
+// Create a variable called random that will generate a random integer greater than 0.
+// Create a variable called mod set to an equation whose remainder is equal to "4".
+// Create a variable called max that will return "20" as the highest number in the set.
