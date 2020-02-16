3b10e4004e5d0c3d1e212f419cb1fb8fcf9bafc4
Mariam Anuashvili <m.anuashvili@gmail.com>
2020-02-16 19:47:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Write your code here
-let var1 = 2;
-let var2 = 31;
+const var1 = 2;
+const var2 = 31;
+
 const multiply = var1 * var2;
 
 const random = Math.random();
@@ -8,11 +9,3 @@ const random = Math.random();
 const mod = 14 % 5; 
 
 const max = Math.max(5, 10, 15, 20);
-
-// describe('max', function() {
-//     it("will return 20 as the highest number in the set", function() {
-//       expect(max).to.eq(20)
-//     });
-//   });
-
-// Create a variable called max that will return "20" as the highest number in the set.
