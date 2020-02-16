a4bf597644e892da3a35e9e44b919281da2e0157
Mariam Anuashvili <m.anuashvili@gmail.com>
2020-02-16 19:50:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here
+const var1 = 2;
+const var2 = 31;
+const multiply = var1 * var2;
+
+const random = Math.random();
+
+const mod = 14 % 5; 
+
+const max = Math.max(5, 10, 15, 20);
